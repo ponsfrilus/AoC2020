ccae00114e77a187dfe69f07a7001a2c48ac4ac5
Nicolas Borboën <ponsfrilus@gmail.com>
2020-12-02 10:42:36 +0100

    [add] Day 01 solution

---
+++ day01/index.js
@@ -0,0 +1,26 @@
+exports.run = (input) => {
+  part1: for (let i = 0; i <= input.length; i++) {
+    for (let j = i; j <= input.length; j++) {
+      let ii = parseInt(input[i])
+      let jj = parseInt(input[j])
+      if (ii + jj == 2020) {
+        console.log('Solution part 1:', ii * jj)
+        break part1
+      }
+    }
+  }
+
+  part2: for (let i = 0; i <= input.length; i++) {
+    for (let j = i; j <= input.length; j++) {
+      for (let k = i; k <= input.length; k++) {
+        let ii = parseInt(input[i])
+        let jj = parseInt(input[j])
+        let kk = parseInt(input[k])
+        if (ii + jj + kk == 2020) {
+          console.log('Solution part 2:', ii * jj * kk)
+          break part2
+        }
+      }
+    }
+  }
+}
